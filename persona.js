64100263048647b836fc5a82e73dc615a45b6edd
Ignacio de Villafañe <idevillafane@gmail.com>
2020-12-29 16:16:39 -0300

    Update persona.js

--- persona.js
+++ persona.js
@@ -18,25 +18,24 @@ var persona = require('../persona');
 POST '/persona' recibe: {nombre: string, apellido: string, alias: string, email: string} retorna: status: 200, {id: numerico, nombre: string, apellido: string, alias: string, email: string} - status: 413, {mensaje: <descripcion del error>} que puede ser: "faltan datos", "el email ya se encuentra registrado", "error inesperado"
 */
 
-app.post('/persona', function (req, res, next) { //cambiar "function" por "async", sacar "next"
+app.post('/persona', async (req, res)) => {
     try {
         if (!req.body.nombre || !req.body.apellido || !req.body.email || !req.body.alias) {
             throw new Error("Faltan datos");
         }
 
-        /* Con estas lineas se deberia poder chequear si ya esta registrado el email:
+        // Con estas lineas se deberia poder chequear si ya esta registrado el email:
 
         var chequearEmail = 'SELECT * FROM persona WHERE email = ?';
         let respuesta = query(query, [req.body.email]);
 
         if (respuesta.length > 0) {
             throw new Error("El email ya se encuentra registrado");
-        }
-
-        */
+        };
     
         query = 'INSERT INTO producto (nombre, apellido, email, alias) VALUES (?, ?, ?, ?)';
-        respuesta = query(query, [req.body.nombre.toUpperCase(), req.body.apellido.toUpperCase(), req.body.email.toUpperCase(), req.body.alias.toUpperCase());
+        
+        respuesta = await qy(query, [req.body.nombre.toUpperCase(), req.body.apellido.toUpperCase(), req.body.email.toUpperCase(), req.body.alias.toUpperCase());
 
         res.send({'respuesta': respuesta.insertId}); //No se si esta línea se escribe así. Lo copie del TP. No sé tampoco si hace falta poner un identificador, creo que no.
         
@@ -48,16 +47,12 @@ app.post('/persona', function (req, res, next) { //cambiar "function" por "async
     }
 });
 
-// GET '/persona' retorna status 200 y [{id: numerico, nombre: string, apellido: string, alias: string, email; string}] o bien status 413 y []
+/*
+Pedir lista de personas:
+GET '/persona' retorna status 200 y [{id: numerico, nombre: string, apellido: string, alias: string, email; string}] o bien status 413 y []
+*/
 
-app.get ('/persona', function (req, res, next)) { //cambiar "function" por "async", sacar "next"
-    try {
-
-    }
-
-GET '/persona/:id' retorna status 200 y {id: numerico, nombre: string, apellido: string, alias: string, email; string} - status 413 , {mensaje: <descripcion del error>} "error inesperado", "no se encuentra esa persona"
-
-app.get ('/persona', function (req, res, next)) { //cambiar "function" por "async", sacar "next"
+app.get ('/persona', async (req, res)) => {
     try {
         var query = 'SELECT * FROM producto';
 
@@ -69,19 +64,38 @@ app.get ('/persona', function (req, res, next)) { //cambiar "function" por "asyn
         res.status(413).send({"Error inesperado": e.message};)
     }
 
+/* GET '/persona/:id' retorna status 200 y {id: numerico, nombre: string, apellido: string, alias: string, email; string} - status 413 , {mensaje: <descripcion del error>} "error inesperado", "no se encuentra esa persona" */
+
+app.get ('/persona/:id', async (req, res)) => {
+    try {
+        const query = 'SELECT * FROM persona WHERE id = ?';
+
+        // const respuesta = await qy()...
+        const respuesta = await qy(query, [req.params.id]);
+
+        console.log(respuesta);
+
+        res.send({"respuesta": respuesta});
+    }
+    catch(e) {
+        console.error(e.message);
+        res.status(413).send({"Error": e.message});
+    }
+
+};
 
 
+/* Lo que sigue es lo que hizo la profesora */
 
-/* TODO LO QUE SIGUE EN LAS PROXIMAS LINEAS CORRESPONDE AL EJEMPLO PRESENTADO POR LA PROFESORA EN EL TP4 DEL MOD3
 
-    var nombre = req.body.nombre;
-    var instancia = new ArtistaModel({ nombre: nombre });
-    instancia.save((err, artista) => {
-        if (err) {
-            next(new Error('No se pudo guardar el artista'));
-        }
-        res.status(201).send(artista); // Codigo HTTP 201 es creado
-    });
+var nombre = req.body.nombre;
+var instancia = new ArtistaModel({ nombre: nombre });
+instancia.save((err, artista) => {
+    if (err) {
+        next(new Error('No se pudo guardar el artista'));
+    }
+    res.status(201).send(artista); // Codigo HTTP 201 es creado
+});
 });
 
 
